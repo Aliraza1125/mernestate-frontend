a813eb076f8a1537bf1babb627910160ab8c5b16
Ali Raza <alirazaasif2511@gmail.com>
2024-10-12 22:52:29 +0600

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
         target: "https://mernestate-backend-3.onrender.com",
-        changeOrigin: true, 
+        changeOrigin: true,
+        secure: false, // Add this line
+        rewrite: (path) => path.replace(/^\/api/, '') // Add this line
       },
     },
   },
